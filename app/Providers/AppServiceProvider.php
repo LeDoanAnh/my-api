a5feb4ca6b09524da40acafdc7f266507d773250
LeDoanAnh <ledoananhhtg2003@gmail.com>
2026-04-07 16:04:27 +0700

    Sửa file AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        \Illuminate\Support\Facades\Schema::defaultStringLength(191);
         // 1. Fix lỗi độ dài key cho database
     //     Schema::defaultStringLength(191);
 
