c8d67b2c0f626f65be65d74241badc607a70cd0a
LeDoanAnh <ledoananhhtg2003@gmail.com>
2026-04-07 16:59:41 +0700

    Sửa file Dockerfile lần 9

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\Log;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -22,18 +18,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        \Illuminate\Support\Facades\Schema::defaultStringLength(191);
-        // 1. Fix lỗi độ dài key cho database
-    //     Schema::defaultStringLength(191);
 
-    // // Chỉ chạy migrate nếu là môi trường production và không phải là chạy qua Terminal
-    //     if (config('app.env') === 'production' && !$this->app->runningInConsole()) {
-    //         try {
-    //             // Thiết lập timeout ngắn để không làm treo tiến trình khởi động của PHP-FPM
-    //             Artisan::call('migrate', ['--force' => true]);
-    //         } catch (\Exception $e) {
-    //             Log::error("Migration error: " . $e->getMessage());
-    //         }
-    //     }
     }
 }
