7b6548e9617c2b3c8f6801efe8e51110775f3a73
LeDoanAnh <ledoananhhtg2003@gmail.com>
2026-04-19 20:43:41 +0700

    create_api_use

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,21 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\User; // Đảm bảo đã có Model User
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index()
     {
-        // Lấy toàn bộ danh sách user từ database
         $users = User::all();
 
-        // Trả về định dạng JSON cho Flutter dễ đọc
         return response()->json([
             'success' => true,
-            'message' => 'Danh sách người dùng',
             'data' => $users
-        ], 200);
+        ]);
     }
 }
