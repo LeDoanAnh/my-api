e65062d581a6c16bfd77193553eac727e386e10a
LeDoanAnh <ledoananhhtg2003@gmail.com>
2026-04-22 16:05:51 +0700

    Hoàn thiện api đăng nhập

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,8 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'role_id' => 'integer',
+            'department_id' => 'integer',
         ];
     }
 }
